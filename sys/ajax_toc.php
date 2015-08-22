e33211cf70a53dc5d07711bc12abf180b9e17383
Tobias Zeumer <github@verweisungsform.de>
2015-08-22 04:29:44 +0200

    Apply caching in ajax for toc. (Is ajax_toc.php necessary anymore?)

--- sys/ajax_toc.php
+++ sys/ajax_toc.php
@@ -1,10 +1,43 @@
 <?php
-$issn = $_GET['issn'];
+$issn = (isset($_GET['issn'])) ? $_GET['issn'] : false;
+$age  = (isset($_GET['age']))  ? $_GET['age']  : -1;
 
-if ($issn) {
+function get_toc($issn) {
   require_once('class.getJournalInfos.php');
   $getInfos = new GetJournalInfos();
-  echo $getInfos->ajax_query_toc($issn);
+  return $getInfos->ajax_query_toc($issn);
+}
+
+if ($issn) {
+  $cachefile = "../cache/toc-$issn.cache.html";
+  
+  // An age is available and cached file exists
+  // (-1 is the same as disabled caching)
+  if ($age > -1 && file_exists($cachefile)) {
+    // Issue age is greater than the age of the cached file - load cache
+    if ($age >= date('a', strtotime(filemtime($cachefile)))) {
+      $toc = file_get_contents($cachefile);
+    }
+    // Otherwise get fresh toc and save to file
+    else {
+      $toc = get_toc($issn);
+      file_put_contents($cachefile, $toc);
+    }
+  }
+  // An age is available but no cached file exists
+  elseif ($age > -1) {
+    $toc = get_toc($issn);
+    file_put_contents($cachefile, $toc);
+  }
+  // Ok, we got no age or caching is disabled by setting it to -1. Get toc the old way
+  else {
+    $toc = get_toc($issn);
+  }
+  
+  // And return it...
+  echo $toc;
+} else {
+  echo "Oops, got no ISSN. this should never ever happen, you know :)"
 }
 
 ?>
--- sys/ajax_toc_fullhtml.php
+++ sys/ajax_toc_fullhtml.php
@@ -1,7 +1,49 @@
 <?php
-$issn = $_GET['issn'];
+$issn = (isset($_GET['issn'])) ? $_GET['issn'] : false;
+$age  = (isset($_GET['age']))  ? $_GET['age']  : -1;
 
 if ($issn) {
+  $cachefile = "../cache/toc-$issn.cache.html";
+  
+  // An age is available and cached file exists
+  // (-1 is the same as disabled caching)
+  if ($age > -1 && file_exists($cachefile)) {
+    // Issue age is greater than the age of the cached file - load cache
+    if ($age >= date('a', strtotime(filemtime($cachefile)))) {
+      $toc = file_get_contents($cachefile);
+    }
+    // Otherwise get fresh toc and save to file
+    else {
+      $toc = get_toc($issn);
+      file_put_contents($cachefile, $toc);
+    }
+  }
+  // An age is available but no cached file exists
+  elseif ($age > -1) {
+    $toc = get_toc($issn);
+    file_put_contents($cachefile, $toc);
+  }
+  // Ok, we got no age or caching is disabled by setting it to -1. Get toc the old way
+  else {
+    $toc = get_toc($issn);
+  }
+  
+  // And return it...
+  echo $toc;
+} else {
+  echo "Oops, got no ISSN. this should never ever happen, you know :)";
+}
+
+
+/**
+ * @brief   Get toc, pad it with some html and put it into an iframe
+ *
+ * @todo    Maybe use CDN for scripts
+ *
+ * @param $issn    \b STR  Journal ISSN
+ * @return \b STR Some html
+ */
+function get_toc($issn) {
   require_once('class.getJournalInfos.php');
   $getInfos = new GetJournalInfos();
   echo '<!DOCTYPE html>
@@ -13,12 +55,13 @@ if ($issn) {
       <script src="../js/vendor/jquery.timeago.js"></script>
 </head><body>';
   $response = $getInfos->ajax_query_toc($issn);
+  
   if (!$response) {
-      $response = '<script>$(document).ready(window.parent.postMessage({"ready": false},"*"));</script></body>';
+    $response = '<script>$(document).ready(window.parent.postMessage({"ready": false},"*"));</script></body>';
   } else {
-      $response .= '<script src="../js/local/frame.js"></script></body>';
+    $response .= '<script src="../js/local/frame.js"></script></body>';
   }
-  echo $response;
+  
+  return $response;
 }
-
 ?>
