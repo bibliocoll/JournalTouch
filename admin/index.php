fe46e4506694eea1d075cee27595e554e07179e7
Tobias Zeumer <github@verweisungsform.de>
2015-08-22 21:21:55 +0200

    Small changes to admin page

--- admin/index.php
+++ admin/index.php
@@ -3,8 +3,7 @@
 <body>
 
 <h1>Update Journal Infos</h1>
-<p>This updates your journals.csv with the dates for each journal issue. If you chose to update, also some infos about publishers, legal infos and tags are fetched, currently as tags for the tagcloud in JournalTouch</p>
-<p>Your options:</p>
+
 
 <?php
 $btn_cache  = (isset($_GET['clr_cache'])) ? true : false;
@@ -34,25 +33,33 @@ if ($btn_cache) {
 
 ?>
 
-<!-- Quick & Dirty - remove cached files -->
-
 <div style="border: thin solid #000000; width:400px; padding:10px;">
   <form method="get" action="index.php">
     <fieldset>
+      <!-- Quick & Dirty - remove cached files -->
       <legend><b>Clear Cache</b></legend>
-      If you enabled the caching in the config, you can clear it here:<br />
+      If you enabled the caching in the config, you can clear it here:<hr />
       <button type="submit" name="clr_cache" value="true">Delete Cache</button> <?php echo $del_message; ?>
     </fieldset>
     <br />
     <fieldset>
+      <!-- Quick & Dirty - Update input.csv -->
       <legend><b>Update options</b></legend>
-      <label for="optMeta"><input type="checkbox" name="optMeta" checked="checked"> Fetch metadata<br>
+      This updates your journals.csv with the dates for each journal issue. If you chose to update, also some infos about publishers, legal infos and tags are fetched, currently as tags for the tagcloud in JournalTouch.<hr />
+      <label for="optMeta"><input type="checkbox" name="optMeta"> Fetch metadata<br>
       <label for="optRecent"><input type="checkbox" name="optRecent" checked="checked"> Fetch recent issues<br>
       <label for="optTags"><input type="checkbox" name="optTags"> Clean tags (experimental)<sup>1</sup><br>
       <button name="upd" value="true" type="submit">Start</button>
       <br /><br />
       <sup>1</sup>You have to edit input/tag-remap.txt. Format: "oldTag;newTag"
     </fieldset>
+    <br />
+    <fieldset>
+      <!-- Quick & Dirty - Update input.csv -->
+      <legend><b>JournalTOC Premium Update</b></legend>
+      This updates your updates.json.txt with the dates for recent issue.<hr />
+      <a href="../services/getLatestJournals.php">Update Premium</a>
+    </fieldset>   
   </form>
 </div>
 
