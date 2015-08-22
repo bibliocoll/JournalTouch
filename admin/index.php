9390658a88bc68020c980c0739e0865c762a3d73
Tobias Zeumer <github@verweisungsform.de>
2015-08-22 05:13:04 +0200

    Added an admin option to remove cached files

--- admin/index.php
+++ admin/index.php
@@ -7,52 +7,28 @@
 <p>Your options:</p>
 
 <?php
-$btn_cache  = (isset($_GET['clr_cache'])) ? true : false;
-$btn_upd    = (isset($_GET['upd'])) ? true : false;
+$upd = (isset($_GET['upd'])) ? true : false;
 $optMeta    = (isset($_GET['optMeta'])) ? true : false;
 $optRecent  = (isset($_GET['optRecent'])) ? true : false;
 $optTags    = (isset($_GET['optTags'])) ? true : false;
 
-if ($btn_upd) {
+if ($upd) {
   require_once('../sys/class.getJournalInfos.php');
   $getInfos = new GetJournalInfos();
   $getInfos->update_journals_csv($optMeta, $optRecent, $optTags);
 }
 
-$del_message = '';
-if ($btn_cache) {
-  $files = glob('../cache/*.cache*'); // get all file names by pattern
-  $i = 0;
-  foreach($files as $file) {
-    if(is_file($file)) {
-      unlink($file);
-      $i++;
-    }
-  }
-  $del_message = "<strong>Sucess: $i files deleted</strong>";
-}
-
 ?>
-
-<!-- Quick & Dirty - remove cached files -->
-
 <div style="border: thin solid #000000; width:400px; padding:10px;">
   <form method="get" action="index.php">
-    <fieldset>
-      <legend><b>Clear Cache</b></legend>
-      If you enabled the caching in the config, you can clear it here:<br />
-      <button type="submit" name="clr_cache" value="true">Delete Cache</button> <?php echo $del_message; ?>
-    </fieldset>
-    <br />
     <fieldset>
       <legend><b>Update options</b></legend>
       <label for="optMeta"><input type="checkbox" name="optMeta" checked="checked"> Fetch metadata<br>
       <label for="optRecent"><input type="checkbox" name="optRecent" checked="checked"> Fetch recent issues<br>
       <label for="optTags"><input type="checkbox" name="optTags"> Clean tags (experimental)<sup>1</sup><br>
-      <button name="upd" value="true" type="submit">Start</button>
-      <br /><br />
-      <sup>1</sup>You have to edit input/tag-remap.txt. Format: "oldTag;newTag"
     </fieldset>
+    <sup>1</sup>You have to edit input/tag-remap.txt. Format: "oldTag;newTag"
+    <button name="upd" value="true" type="submit">Start</button>
   </form>
 </div>
 
