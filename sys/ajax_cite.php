0dc975205381580a5c472730edcb96db758659d7
Tobias Zeumer <github@verweisungsform.de>
2015-11-29 16:10:21 +0100

    Replace curl by file_get_contents

--- sys/ajax_cite.php
+++ sys/ajax_cite.php
@@ -1,14 +1,15 @@
 <?php
-$doi = $_GET['doi'];
+$doi = (isset($_GET['doi'])) ? $_GET['doi'] : false;
+
 if ($doi) {
-    $ch = curl_init("http://dx.doi.org/".$doi); //yep, oldschool
-    /*curl_setopt($ch, CURLOPT_ENCODING, 'application/vnd.citationstyles.csl+json');*/
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/vnd.citationstyles.csl+json, application/rdf+xml'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    //For Debugging
-    //curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
-    echo curl_exec($ch);
-    curl_close($ch);
+    $url = "http://dx.doi.org/$doi";
+
+	$context = stream_context_create(array(
+        'http' => array('header' => 'Accept: application/vnd.citationstyles.csl+json, application/rdf+xml')
+    ));
+	$cr_json = file_get_contents($url, false, $context);
+
+    echo $cr_json;
+
 }
 ?>
